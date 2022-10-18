9823c2aa5596147dcc087f50fa5cf4417c6e2c49
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-18 14:37:54 -0500

    primer comit ws

---
+++ server-echo/server.js
@@ -0,0 +1,30 @@
+const WebSocket=require("ws"); //se crea una constante que requiere la libreria ws
+const PORT=5000;
+const wsServer=new WebSocket.Server({
+    port: PORT
+});
+
+//Mediante javascripts podemos implementar eventos
+//se define el comportamiento cuando se levanta una conexion
+wsServer.on('connection',function(socket){
+    console.log("cliente conectado");
+
+    //Podemos definir algun comportamiento al conectarse el socket
+    socket.on('message',function(msg){
+        console.log("Mensaje recibido por el cliente: "+msg);
+        //socket.send("Escribiste: "+msg); //envia el mensaje al cliente conectado
+
+        //Broadcast del mensaje enviado
+        wsServer.clients.forEach(function(client){
+            client.send("Alguien envio esto:" + msg);
+        });
+
+
+    });
+});
+
+console.log((new Date())+" Server escuchando en puerto "+PORT);
+
+
+
+
