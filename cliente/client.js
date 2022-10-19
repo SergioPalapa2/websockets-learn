54a04eda42ddc604d9a169f25793ce5ec93c47dd
SergioPalapa2 <sergio.palapa.o@gmail.com>
2022-10-18 22:09:27 -0500

    ws-cliente-serv home

--- cliente/client.js
+++ cliente/client.js
@@ -1,2 +1,17 @@
-//aqui se programara el cliente
-//continuara...
+const WebSocket=require("ws");
+const serverAdress="ws://127.0.0.1:5000";
+
+const ws=new WebSocket(serverAdress) //se crea un web socket que se conecta a la direccion
+
+//accion si se tiene una conexion correcta
+ws.on('open',function(){
+    ws.send("Hola server");
+}); 
+
+ws.on('message', function(msg){
+    console.log("Mensaje recibido del server"+msg);
+
+})
+
+
+
--- server-echo/server.js
+++ server-echo/server.js
@@ -6,17 +6,17 @@ const wsServer=new WebSocket.Server({
 //Mediante javascripts podemos implementar eventos
 //se define el comportamiento cuando se levanta una conexion
 wsServer.on('connection',function(socket){
-    console.log("cliente conectado");
+    console.log("Cliente conectado");
 
     //Podemos definir algun comportamiento al conectarse el socket
     socket.on('message',function(msg){
-        console.log("Mensaje recibido por el cliente: "+msg);
-        //socket.send("Escribiste: "+msg); //envia el mensaje al cliente conectado
+        
+        socket.send("Tu mensaje fue: "+msg); //envia el mensaje al cliente conectado
 
-        //Broadcast del mensaje enviado
-        wsServer.clients.forEach(function(client){
-            client.send("Alguien envio esto:" + msg);
-        });
+        // //Broadcast del mensaje enviado
+        // wsServer.clients.forEach(function(client){
+        //     client.send("Alguien envio esto:" + msg);
+        // });
 
 
     });
